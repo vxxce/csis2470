dca9c94b1952bb33e0e218b1b7056cbce61f75fc
Zachary Olpin <info@zacharyolp.in>
2020-07-25 06:04:04 -0600

    reduce hobbies to string, add comments

--- public/h/3/js/slideShow.js
+++ public/h/3/js/slideShow.js
@@ -1,27 +1,20 @@
-// When DOM loaded, execute parameterized callback
+// When DOM loaded, execute callback
 $(document).ready(function() {
-  let html = ""
-  // Load hobbies.json data asynchronously
-  $.getJSON('./js/hobbies.json', function(json) {
-    // Loop over data
-    $.each(json, function(k, hobbies) {
-      // Create a new div from each hobby object in hobbies array and append to html string
-      $.each(hobbies, function(k, hobby) {
-        html += "<div>"
-        html += `<h2>${hobby.name}</h2>`
-        html += `<img src="./img/${hobby.src}" />`
-        html += `<p>${hobby.description}</p>`
-        html += "</div>"
-      })
-    })
-    // Make slideshow by cycling through each top-level element in html string (the hobby divs)
+  // Load JSON data with Jquery shorthand ajax method
+  $.getJSON('./js/hobbies.json', function(res) {
+    // Reduce array of hobby objects to an html string of divs, each with a header, image, and caption.
+    let html = res['hobbies'].reduce(function(acc, h) {
+      return acc.concat(`<div><h2>${h.name}</h2><img src="./img/${h.src}" /><p>${h.description}</p></div>`)
+    }, "")
+
+    // Use Jquery `cycle` plugin to make slideshow from divs in the html string.
     $('#hobbies').html(html).cycle({
       fx: "scrollHorz", // Transition effect
       prev: "#prev", // Selector to cycle to previous
       next: "#next", // Select to cycle to next
       sync: true, // Transition slides in sync
       pause: true, // Pause playback on hover
-      timeout: 8000, // 8 second default for each slide
+      timeout: 6000, // 8 second default for each slide
     })
   })
 })
