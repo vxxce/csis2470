2452a5cc63ea523a0a63fbe7da540552fb1538a0
Zachary Olpin <info@zacharyolp.in>
2020-07-24 23:31:18 -0600

    move jquery read callback into own file

---
+++ public/h/3/js/slideShow.js
@@ -0,0 +1,27 @@
+// When DOM loaded, execute parameterized callback
+$(document).ready(function() {
+  let html = ""
+  // Load hobbies.json data asynchronously
+  $.getJSON('./js/hobbies.json', function(json) {
+    // Loop over data
+    $.each(json, function(k, hobbies) {
+      // Create a new div from each hobby object in hobbies array and append to html string
+      $.each(hobbies, function(k, hobby) {
+        html += "<div>"
+        html += `<h2>${hobby.name}</h2>`
+        html += `<img src="./img/${hobby.src}" />`
+        html += `<p>${hobby.description}</p>`
+        html += "</div>"
+      })
+    })
+    // Make slideshow by cycling through each top-level element in html string (the hobby divs)
+    $('#hobbies').html(html).cycle({
+      fx: "scrollHorz", // Transition effect
+      prev: "#prev", // Selector to cycle to previous
+      next: "#next", // Select to cycle to next
+      sync: true, // Transition slides in sync
+      pause: true, // Pause playback on hover
+      timeout: 6000, // 6 second default for each slide
+    })
+  })
+})
